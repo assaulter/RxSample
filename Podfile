2ed3d030e15980275156a0cc9d8013b3e49d60da
Kazuki Kubo <kz@assaulter.org>
2018-08-31 22:38:51 +0900

    - Add CocoaPod - install RxSwift - install RxCocoa - add sample

---
+++ Podfile
@@ -0,0 +1,19 @@
+use_frameworks!
+
+project 'RxSample.xcodeproj'
+
+def rx_pod    
+    pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+end
+
+def rx_testing_pod
+    pod 'RxBlocking', '~> 4.0'
+    pod 'RxTest',     '~> 4.0'
+end
+
+target 'RxSample' do project 'RxSample.xcodeproj'
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  # Pods for Renosy
+  rx_pod
+end
--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initBindings()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+// MARK: - bindings
+extension ViewController {
+    func initBindings() {
+        button.rx.tap.asDriver().drive(onNext: { () in
+            print("hoge")
+        }, onCompleted: nil, onDisposed: nil).disposed(by: disposeBag)
+    }
+}
