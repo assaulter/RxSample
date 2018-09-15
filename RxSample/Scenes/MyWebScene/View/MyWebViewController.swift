25f6b67c98723e5e4e6825161dfee55acf7b41eb
Kazuki Kubo <kz@assaulter.org>
2018-09-16 00:05:04 +0900

    show progress bar.

--- RxSample/Scenes/MyWebScene/View/MyWebViewController.swift
+++ RxSample/Scenes/MyWebScene/View/MyWebViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 import Foundation
 import RxSwift
+import WebKit
 
 class MyWebViewController: UIViewController {
     var viewModel: MyWebViewModelType!
     var cancelButton: UIBarButtonItem?
+    var webView: WKWebView!
+    var progressBar: UIProgressView?
     private let disposeBag = DisposeBag()
+
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +33,30 @@ class MyWebViewController: UIViewController {
         initializeView()
         initializeBinding()
     }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if let path = keyPath {
+            switch path {
+            case "estimatedProgress":
+                print(Float(webView.estimatedProgress))
+                progressBar?.setProgress(Float(webView.estimatedProgress), animated: true)
+            case "loading":
+                UIApplication.shared.isNetworkActivityIndicatorVisible = webView.isLoading
+                if webView.isLoading {
+                    progressBar?.setProgress(0.1, animated: true)
+                } else {
+                    progressBar?.setProgress(0, animated: false)
+                }
+            default:
+                return
+            }
+        }
+    }
+
+    deinit {
+        webView.removeObserver(self, forKeyPath: "loading")
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
 }
 
 extension MyWebViewController: BaseViewType {
@@ -30,11 +64,33 @@ extension MyWebViewController: BaseViewType {
         cancelButton?.rx.tap.asDriver().drive(onNext: { [weak self] (_) in
             self?.viewModel.input.dismissMyWebView()
         }).disposed(by: disposeBag)
+
+        // binding...?
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
     }
     
     func initializeView() {
         navigationItem.title = "MyWeb"
         cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: nil)
         navigationItem.rightBarButtonItem = cancelButton
+        if let _navigationController = navigationController {
+            progressBar = UIProgressView(frame: CGRect(x: 0, y: (_navigationController.navigationBar.frame.height) - 2, width: _navigationController.navigationBar.frame.width, height: 10))
+            progressBar?.progressViewStyle = .bar
+            navigationController?.navigationBar.addSubview(progressBar!)
+        }
+        // load
+        load()
     }
+
+    private func load() {
+        let urlStr = "https://www.renosy.com/webview/term"
+        let url = URL(string: urlStr)
+        if let _url = url {
+            webView.load(URLRequest(url: _url))
+        }
+    }
+}
+
+extension MyWebViewController: WKUIDelegate {
 }
